11837de53db1deba3bf7c10b1b73aac9ad810a77
ed.longman <ed@eds-Mac-Pro.local>
2012-11-05 19:45:32 +0000

    Initial Commit

---
+++ Rpi/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Rpi
+//
+//  Created by ed on 05/11/2012.
+//  Copyright (c) 2012 ed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
