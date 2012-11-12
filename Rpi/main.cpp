0a4627ff6ffb38f7c93225dd31ac6cb55e7eaae8
ed.longman <ed@eds-Mac-Pro.local>
2012-11-12 20:20:23 +0000

    Initial commit of files, Done some basic hacking together

--- Rpi/main.cpp
+++ Rpi/main.cpp
@@ -5,14 +5,15 @@
 //  Created by ed on 05/11/2012.
 //  Copyright (c) 2012 ed. All rights reserved.
 //
+//#include "util.h"
+//#include "file.h"
+//#include "log.h"
 
 #include <iostream>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    //testing my RPi GPIO
+    
 }
 
---
+++ util.h
@@ -0,0 +1,14 @@
+//
+//  util.h
+//  Rpi
+//
+//  Created by ed on 05/11/2012.
+//  Copyright (c) 2012 ed. All rights reserved.
+//
+
+#ifndef Rpi_util_h
+#define Rpi_util_h
+
+
+
+#endif
