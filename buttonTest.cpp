3db2f91ebdb14b5f573fe35a9ba66510ca7f23ba
ed.longman <ed@eds-Mac-Pro.local>
2012-11-20 20:31:30 +0000

    created a simple button tester

---
+++ buttonTest.cpp
@@ -0,0 +1,28 @@
+//
+//  buttonTest.cpp
+//  Rpi
+//
+//  Created by ed on 20/11/2012.
+//  Copyright (c) 2012 ed. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "GPIO/wiringPi.h"
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    //testing my RPi GPIO
+    cout << "GPIO Test Program.\n";
+    
+    if (wiringPiSetup () == -1){
+        cout << "couldn't setup GPIO";
+        exit (1) ;
+    }
+    pinMode(0,INPUT);
+    while(digitalRead(0)==0){
+        cout << "pressed.\n";
+    }
+}
