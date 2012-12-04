b3a3fa463bb9e1f5792f40401f5e93acc78f6968
ed.longman <ed@eds-Mac-Pro.local>
2012-12-04 22:26:44 +0000

    added averaging for the sensor and changed the delays

--- voltageOutput.cpp
+++ voltageOutput.cpp
@@ -32,17 +32,22 @@ int main(int argc, const char * argv[])
     //setup chip
     atod sensor(14,10,13,12); //clock,shutdown,dout,din
     //does a test read of the sensor
-    for (int i=0; i<10000 ; i++){
+    for (int i=0; i<1000 ; i++){
         double cumulative_voltage=0;
+        int cumulative_sensor=0;
         for (int ii=0;ii<10;ii++){
             int sensor_val=sensor.read();
             double voltage=(sensor_val*3.3)/1024;
             cumulative_voltage+=voltage;
-            delay(5); //slows the program down!
+            cumulative_sensor+=sensor_val;
+            delay(1); //slows the program down!
         }
         cumulative_voltage=cumulative_voltage*100;
         cumulative_voltage=floor(cumulative_voltage);
         cumulative_voltage=cumulative_voltage/1000;
+        cumulative_sensor=cumulative_sensor*100;
+        cumulative_sensor=floor(cumulative_sensor);
+        cumulative_sensor=cumulative_sensor/1000;
         printf("Reading : %u     Voltage : %f",sensor.read(),cumulative_voltage);
         putchar(0x0D); // go to start of the line
         delay(10); //slows the program down!
