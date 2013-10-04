ae79c034c5829323cc1f37d98310ae1803a2b6e7
Edward Longman <ed@edlog.co.uk>
2013-10-04 16:55:37 +0100

    switched to arduinogit add Pi_Data_Logger.ino

--- Pi_Data_Logger.ino
+++ Pi_Data_Logger.ino
@@ -1,1 +1,30 @@
+#include <Wire.h>
+#include <SD.h>
 
+//
+//  main.cpp
+//  RPi
+//
+//  Created by ed on 04/05/2013.
+//  Copyright (c) 2013 ed. All rights reserved.
+//
+  // This is the I2C address
+#include "sensor.h"
+
+temperature temp1(0);
+light light1(0);
+unsigned long currTime;
+void setup(){
+    //start clock
+    //create sensors
+    temperature temp1(0);
+    light light1(0);
+    unsigned long currTime=micros();
+    SD.begin(10);
+}
+void loop(){
+    double currTemp;
+    bool tempResult=temp1.log(currTime,&currTemp);
+    double currLight;
+    bool lightResult=light1.log(currTime,&currLight);
+}
